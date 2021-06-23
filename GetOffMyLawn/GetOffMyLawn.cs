61b718f79deef77d7a3f74f6861941580529fa9a
redseiko <redseiko@gmail.com>
2021-06-23 16:37:45 -0700

    Updated GetOffMyLawn for patch v0.155.7.
      * Refactored code to use local constants and re-use data structures.
      * Fixed a bug where plants/saplings would also have their heath values modified.

--- GetOffMyLawn/GetOffMyLawn.cs
+++ GetOffMyLawn/GetOffMyLawn.cs
@@ -2,90 +2,91 @@
 using BepInEx.Configuration;
 using HarmonyLib;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace GetOffMyLawn {
-  [BepInPlugin(GetOffMyLawn.Package, GetOffMyLawn.ModName, GetOffMyLawn.Version)]
+  [BepInPlugin(Package, ModName, Version)]
   public class GetOffMyLawn : BaseUnityPlugin {
     public const string Package = "redseiko.valheim.getoffmylawn";
-    public const string Version = "0.0.6";
+    public const string Version = "0.1.1";
     public const string ModName = "Get Off My Lawn";
 
-    private static ConfigEntry<bool> isModEnabled;
-    private static ConfigEntry<float> pieceHealth;
+    private static ConfigEntry<bool> _isModEnabled;
+    private static ConfigEntry<float> _pieceHealth;
 
-    private static ConfigEntry<bool> showTopLeftMessageOnPieceRepair;
-    private static ConfigEntry<bool> showRepairEffectOnWardActivation;
+    private static ConfigEntry<bool> _showTopLeftMessageOnPieceRepair;
+    private static ConfigEntry<bool> _showRepairEffectOnWardActivation;
 
-    private readonly Harmony harmony = new Harmony("redseiko.valheim.getoffmylawn");
+    private Harmony _harmony;
 
-    public void Awake() {
-      isModEnabled = Config.Bind("Global", "isModEnabled", true, "Whether the mod should be enabled.");
+    private void Awake() {
+      _isModEnabled = Config.Bind("Global", "isModEnabled", true, "Whether the mod should be enabled.");
 
-      pieceHealth =
+      _pieceHealth =
           Config.Bind(
               "PieceValues",
               "pieceHealth",
               100000f,
               "Target value to set piece health to when creating and repairing.");
 
-      showTopLeftMessageOnPieceRepair =
+      _showTopLeftMessageOnPieceRepair =
           Config.Bind(
               "Indicators",
               "showTopLeftMessageOnPieceRepair",
               false,
               "Shows a message in the top-left message area on piece repair.");
 
-      showRepairEffectOnWardActivation =
+      _showRepairEffectOnWardActivation =
           Config.Bind(
               "Indicators",
               "showRepairEffectOnWardActivation",
               false,
               "Shows the repair effect on affected pieces when activating a ward.");
 
-      harmony.PatchAll();
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
     }
 
-    public void OnDestroy() {
-      harmony.UnpatchSelf();
+    private void OnDestroy() {
+      if (_harmony != null) {
+        _harmony.UnpatchSelf();
+      }
     }
 
     [HarmonyPatch(typeof(BaseAI))]
-    class BaseAiPatch {
+    private class BaseAiPatch {
+      private static readonly string[] _targetRayMask = new string[] {
+        "Default", "static_solid", "Default_small", "vehicle",
+      };
+
       [HarmonyPostfix]
       [HarmonyPatch(nameof(BaseAI.Awake))]
       private static void AwakePostfix(ref BaseAI __instance) {
-        if (!isModEnabled.Value) {
-          return;
+        if (_isModEnabled.Value) {
+          __instance.m_monsterTargetRayMask = LayerMask.GetMask(_targetRayMask);
         }
+      }
+    }
 
-        __instance.m_monsterTargetRayMask = LayerMask.GetMask(
-            new string[] {
-                "Default",
-                "static_solid",
-                "Default_small",
-                "vehicle"
-            });
+    [HarmonyPatch(typeof(MonsterAI))]
+    private class MonsterAiPatch {
+      [HarmonyPrefix]
+      [HarmonyPatch(nameof(MonsterAI.UpdateTarget))]
+      private static void UpdateTargetPrefix(ref MonsterAI __instance) {
+        if (_isModEnabled.Value) {
+          __instance.m_attackPlayerObjects = false;
+        }
       }
     }
 
     [HarmonyPatch(typeof(Piece))]
-    class PiecePatch {
-      [HarmonyPrefix]
-      [HarmonyPatch(nameof(Piece.IsValidMonsterTarget))]
-      private static bool IsValidMonsterTargetPrefix(ref bool __result) {
-        if (!isModEnabled.Value) {
-          return true;
-        }
-
-        __result = false;
-        return false;
-      }
+    private class PiecePatch {
+      private static readonly int _healthHashcode = "health".GetStableHashCode();
 
       [HarmonyPostfix]
       [HarmonyPatch(nameof(Piece.SetCreator))]
       private static void SetCreatorPostfix(ref Piece __instance) {
-        if (!isModEnabled.Value || !__instance || !__instance.m_nview) {
+        if (!_isModEnabled.Value || !__instance || !__instance.m_nview || __instance.GetComponent<Plant>()) {
           return;
         }
 
@@ -93,98 +94,94 @@ namespace GetOffMyLawn {
             || __instance.m_category == Piece.PieceCategory.Building
             || __instance.m_category == Piece.PieceCategory.Crafting
             || __instance.m_category == Piece.PieceCategory.Furniture) {
-          string pieceName = Localization.instance.Localize(__instance.m_name);
-          ZLog.Log("Creating piece '" + pieceName + "' with health: " + pieceHealth.Value);
+          ZLog.Log(string.Format(
+              "Creating piece '{0}' with health: {1}",
+              Localization.instance.Localize(__instance.m_name),
+              _pieceHealth.Value));
 
-          __instance.m_nview.GetZDO().Set("health", pieceHealth.Value);
+          __instance.m_nview.GetZDO().Set(_healthHashcode, _pieceHealth.Value);
         }
       }
     }
 
     [HarmonyPatch(typeof(PrivateArea))]
-    class PrivateAreaPatch {
+    private class PrivateAreaPatch {
+      private static readonly int _healthHashcode = "health".GetStableHashCode();
+      private static readonly List<Piece> _pieces = new List<Piece>();
+      private static int _pieceCount = 0;
+
       [HarmonyPostfix]
       [HarmonyPatch(nameof(PrivateArea.Interact))]
       private static void InteractPostfix(ref PrivateArea __instance) {
-        if (!isModEnabled.Value) {
+        if (!_isModEnabled.Value || !__instance || !__instance.IsEnabled() || !__instance.m_piece.IsCreator()) {
           return;
         }
 
-        if (!__instance.m_piece.IsCreator()) {
-          return;
-        }
+        _pieces.Clear();
+        _pieceCount = 0;
 
-        if (!__instance.IsEnabled()) {
-          return;
-        }
+        Piece.GetAllPiecesInRadius(__instance.transform.position, __instance.m_radius, _pieces);
 
-        List<Piece> pieces = new List<Piece>();
-        Piece.GetAllPiecesInRadius(__instance.transform.position, __instance.m_radius, pieces);
+        foreach (var piece in _pieces) {
+          if (piece.GetComponent<Plant>()) {
+            continue;
+          }
 
-        int pieceCount = 0;
-
-        foreach (var piece in pieces) {
           if (piece.m_category == Piece.PieceCategory.Misc
               || piece.m_category == Piece.PieceCategory.Building
               || piece.m_category == Piece.PieceCategory.Crafting
               || piece.m_category == Piece.PieceCategory.Furniture) {
-            piece.m_nview.GetZDO().Set("health", pieceHealth.Value);
+            piece.m_nview.GetZDO().Set(_healthHashcode, _pieceHealth.Value);
 
-            if (showRepairEffectOnWardActivation.Value) {
+            if (_showRepairEffectOnWardActivation.Value) {
               piece.m_placeEffect.Create(piece.transform.position, piece.transform.rotation);
             }
 
-            pieceCount++;
+            _pieceCount++;
           }
         }
 
-        ZLog.Log("Repairing " + pieceCount + " pieces to health: " + pieceHealth.Value);
+        ZLog.Log(string.Format("Repaired {0} pieces to health: {1}", _pieceCount, _pieceHealth.Value));
 
-        if (showTopLeftMessageOnPieceRepair.Value) {
+        if (_showTopLeftMessageOnPieceRepair.Value) {
           Player.m_localPlayer.Message(
               MessageHud.MessageType.TopLeft,
-              "Repaired '" + pieceCount + "' pieces to health: " + pieceHealth.Value);
+              string.Format("Repaired {0} pieces to health: {1}", _pieceCount, _pieceHealth.Value));
         }
       }
     }
 
     [HarmonyPatch(typeof(Player))]
-    class PlayerPatch {
+    private class PlayerPatch {
+      private static readonly int _healthHashcode = "health".GetStableHashCode();
+
       [HarmonyPrefix]
       [HarmonyPatch(nameof(Player.Repair))]
       private static void RepairPrefix(ref Player __instance, ItemDrop.ItemData toolItem, Piece repairPiece) {
-        if (!isModEnabled.Value) {
+        if (!_isModEnabled.Value || !__instance || !__instance.InPlaceMode()) {
           return;
         }
 
-        if (!__instance.InPlaceMode()) {
-          return;
-        }
+        Piece hoveringPiece = __instance.m_hoveringPiece;
 
-        Piece hoveringPiece = __instance.GetHoveringPiece();
-
-        if (hoveringPiece == null) {
-          return;
-        }
-
-        if (!__instance.CheckCanRemovePiece(hoveringPiece)) {
+        if (!hoveringPiece || !__instance.CheckCanRemovePiece(hoveringPiece) || hoveringPiece.GetComponent<Plant>()) {
           return;
         }
 
         string pieceName = Localization.instance.Localize(hoveringPiece.m_name);
 
-        if (!PrivateArea.CheckAccess(hoveringPiece.transform.position, 0f, /*flash=*/ false, /*wardCheck=*/ false)) {
-          ZLog.Log("Unable to repair piece '" + pieceName + "' due to ward in range.");
+        if (!PrivateArea.CheckAccess(hoveringPiece.transform.position, radius: 0f, flash: false, wardCheck: false)) {
+          ZLog.Log(string.Format("Unable to repair piece '{0}' due to ward in range.", pieceName));
           return;
         }
 
-        ZLog.Log("Repairing piece '" + pieceName + "' to health: " + pieceHealth.Value);
-        hoveringPiece.m_nview.GetZDO().Set("health", pieceHealth.Value);
+        hoveringPiece.m_nview.GetZDO().Set(_healthHashcode, _pieceHealth.Value);
+        ZLog.Log(string.Format("Repaired piece '{0}' to health: {1}", pieceName, _pieceHealth.Value));
 
-        if (showTopLeftMessageOnPieceRepair.Value) {
-            __instance.Message(
+        if (_showTopLeftMessageOnPieceRepair.Value) {
+          __instance.Message(
               MessageHud.MessageType.TopLeft,
-              "Repaired piece " + pieceName + " to health: " + pieceHealth.Value);
+              string.Format("Repaired piece '{0}' to health: {1}", pieceName, _pieceHealth.Value));
         }
       }
     }
--- GetOffMyLawn/Properties/AssemblyInfo.cs
+++ GetOffMyLawn/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GetOffMyLawn")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
