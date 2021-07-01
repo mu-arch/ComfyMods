c92d233948044171a8b53e24cec37f65bb089824
redseiko <redseiko@gmail.com>
2021-06-30 20:13:41 -0700

    Create ColorfulLights mod that enables coloring torches and fireplaces.
      * Fireworks launched from colored bonfires will be the same color.
      * Players without the mod will still see yellow fire and yellow fireworks.

---
+++ ColorfulLights/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using ColorfulLights;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ColorfulLights")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ColorfulLights")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b47b8f8-811b-488b-a8ee-d20ec60f25f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ColorfulLights.ColorfulLights.PluginVersion)]
+[assembly: AssemblyFileVersion(ColorfulLights.ColorfulLights.PluginVersion)]
---
+++ ColorfulLights/README.md
@@ -0,0 +1,13 @@
+﻿# ColorfulLights
+
+  * You can color any torch, firepit, stone hearth and bonfire using RGB and HTML color codes!
+  * Fireworks launched from bonfires will also be colored **AND** visible to others with the mod.
+  * Those without the mod will still see yellow torches, green guck torches and standard yellow fireworks.
+
+# Instructions
+
+  1. Unzip `ColorfulLights.dll` to your `/Valheim/BepInEx/plugins/` folder.
+  2. In-game, press F1 to bring up the ConfigurationManager and navigate to the ColorfulLights section.
+     * Change the target color using the RGB sliders or using an HTML color code.
+  3. Hover or any torch or fireplace and a prompt to change its color will appear.
+  4. Hit `LeftShift + E` to change the color.
