b0bb11b01cbe8a0e2ff0a8f81dd3c139b9d77fb1
redseiko <redseiko@gmail.com>
2021-08-05 13:26:03 -0700

    Modify LetMePlay to use new project template and bump it to 1.0.0.
      * Add a sprite cache (even if it's for only one thing) for GetSprite().

--- LetMePlay/LetMePlay.cs
+++ LetMePlay/LetMePlay.cs
@@ -2,6 +2,7 @@
 using BepInEx.Configuration;
 using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
@@ -10,7 +11,7 @@ namespace LetMePlay {
   [BepInPlugin(Package, ModName, Version)]
   public class LetMePlay : BaseUnityPlugin {
     public const string Package = "redseiko.valheim.letmeplay";
-    public const string Version = "0.0.5";
+    public const string Version = "1.0.0";
     public const string ModName = "Let Me Play";
 
     private static ConfigEntry<bool> _isModEnabled;
@@ -20,7 +21,7 @@ namespace LetMePlay {
 
     private Harmony _harmony;
 
-    private void Awake() {
+    public void Awake() {
       _isModEnabled = Config.Bind("Global", "isModEnabled", true, "Globally enable or disable this mod.");
 
       _disableWardShieldFlash =
@@ -39,17 +40,27 @@ namespace LetMePlay {
       _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
     }
 
-    private void OnDestroy() {
-      if (_harmony != null) {
-        _harmony.UnpatchSelf();
+    public void OnDestroy() {
+      _harmony?.UnpatchSelf();
+    }
+
+    private static readonly Dictionary<string, Sprite> _spriteCache = new();
+
+    private static Sprite GetSprite(string spriteName) {
+      if (!_spriteCache.TryGetValue(spriteName, out Sprite sprite)) {
+        sprite = Resources.FindObjectsOfTypeAll<Sprite>().First(obj => obj.name == spriteName);
+        _spriteCache[spriteName] = sprite;
       }
+
+
+      return sprite;
     }
 
     [HarmonyPatch(typeof(PrivateArea))]
     private class PrivateAreaPatch {
       [HarmonyPrefix]
       [HarmonyPatch(nameof(PrivateArea.RPC_FlashShield))]
-      private static bool PrivateAreaRpcFlashShield(PrivateArea __instance) {
+      private static bool PrivateAreaRpcFlashShield() {
         if (_isModEnabled.Value && _disableWardShieldFlash.Value) {
           return false;
         }
@@ -63,30 +74,28 @@ namespace LetMePlay {
       [HarmonyPrefix]
       [HarmonyPatch(nameof(ItemDrop.ItemData.GetIcon))]
       private static void ItemDataGetIcon(ref ItemDrop.ItemData __instance) {
-        if (_isModEnabled.Value) {
-          if (__instance.m_variant < 0 || __instance.m_variant >= __instance.m_shared.m_icons.Length) {
-            Array.Resize(ref __instance.m_shared.m_icons, __instance.m_variant + 1);
+        if (!_isModEnabled.Value) {
+          return;
+        }
 
-            __instance.m_shared.m_icons[__instance.m_variant] = GetSprite("hammer_icon_small");
-            __instance.m_shared.m_name = __instance.m_dropPrefab.name;
-            __instance.m_shared.m_description = "Non-player item: " + __instance.m_dropPrefab.name;
-            __instance.m_shared.m_itemType = ItemDrop.ItemData.ItemType.Misc;
-            __instance.m_crafterID = 12345678L;
-            __instance.m_crafterName = "redseiko.valheim.letmeplay";
-          }
+        if (__instance.m_variant < 0 || __instance.m_variant >= __instance.m_shared.m_icons.Length) {
+          Array.Resize(ref __instance.m_shared.m_icons, __instance.m_variant + 1);
+
+          __instance.m_shared.m_icons[__instance.m_variant] = GetSprite("hammer_icon_small");
+          __instance.m_shared.m_name = __instance.m_dropPrefab.name;
+          __instance.m_shared.m_description = "Non-player item: " + __instance.m_dropPrefab.name;
+          __instance.m_shared.m_itemType = ItemDrop.ItemData.ItemType.Misc;
+          __instance.m_crafterID = 12345678L;
+          __instance.m_crafterName = "redseiko.valheim.letmeplay";
         }
       }
-    }
-
-    private static Sprite GetSprite(string spriteName) {
-      return Resources.FindObjectsOfTypeAll<Sprite>().FirstOrDefault(obj => obj.name == spriteName);
     }
 
     [HarmonyPatch(typeof(GameCamera))]
     private class GameCameraPatch {
       [HarmonyPostfix]
       [HarmonyPatch(nameof(GameCamera.GetCameraBaseOffset))]
-      private static void GetCameraBaseOffsetPostfix(GameCamera __instance, Player player, ref Vector3 __result) {
+      private static void GetCameraBaseOffsetPostfix(ref Vector3 __result, Player player) {
         if (_isModEnabled.Value && _disableCameraSwayWhileSitting.Value) {
           __result = player.m_eye.transform.position - player.transform.position;
         }
