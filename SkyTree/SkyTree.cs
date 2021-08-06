5a7e2c2be5a30412b426fcf277d4d9cb70e96912
redseiko <redseiko@gmail.com>
2021-08-05 17:54:59 -0700

    Update SkyTree project template to use latest DLLs and minor code clean-up.

--- SkyTree/SkyTree.cs
+++ SkyTree/SkyTree.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
 using System;
 using System.Collections;
@@ -11,28 +12,29 @@ namespace SkyTree {
   public class SkyTree : BaseUnityPlugin {
     public const string PluginGuid = "redseiko.valheim.skytree";
     public const string PluginName = "SkyTree";
-    public const string PluginVersion = "1.0.0";
+    public const string PluginVersion = "1.1.0";
 
     private static ConfigEntry<bool> _isModEnabled;
 
+    private static ManualLogSource _logger;
     private Harmony _harmony;
 
     private void Awake() {
       _isModEnabled = Config.Bind("_Global", "isModEnabled", true, "Globally enable or disable this mod.");
-      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+
+      _logger = Logger;
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
     }
 
     private void OnDestroy() {
-      if (_harmony != null) {
-        _harmony.UnpatchSelf();
-      }
+      _harmony?.UnpatchSelf();
     }
 
     [HarmonyPatch(typeof(ZNetScene))]
-    private class ZNetScenePatch {
+    class ZNetScenePatch {
       [HarmonyPostfix]
       [HarmonyPatch(nameof(ZNetScene.Awake))]
-      private static void AwakePostfix(ZNetScene __instance) {
+      static void AwakePostfix(ZNetScene __instance) {
         if (_isModEnabled.Value) {
           __instance.StartCoroutine(FixYggdrasilBranchCoroutine());
         }
@@ -40,49 +42,48 @@ namespace SkyTree {
     }
 
     private static IEnumerator FixYggdrasilBranchCoroutine() {
-      ZLog.Log("Starting FixYggdrasilBranch coroutine.");
+      _logger.LogInfo("Starting FixYggdrasilBranch coroutine.");
+      WaitForSeconds waitInterval = new(seconds: 3f);
 
       while (true) {
-        yield return new WaitForSeconds(3);
+        yield return waitInterval;
 
         if (!ZNetScene.instance) {
           continue;
         }
 
-        GameObject[] objects = Array.FindAll(
+        GameObject[] yggdrasils = Array.FindAll(
             FindObjectsOfType<GameObject>(), obj => obj.name.StartsWith("YggdrasilBranch"));
 
-        if (objects.Length <= 0) {
+        if (yggdrasils.Length < 1) {
           continue;
         }
 
-        foreach (var yggdrasil in objects) {
-          ZLog.Log("Setting YggdrasilBranch layer to 15.");
+        foreach (GameObject yggdrasil in yggdrasils) {
+          _logger.LogInfo("Setting YggdrasilBranch layer to 15.");
           yggdrasil.layer = 15;
 
-          var branch = yggdrasil.transform.Find("branch");
+          Transform branch = yggdrasil.transform.Find("branch");
 
           if (!branch) {
             continue;
           }
 
-          ZLog.Log("Found YggdrasilBranch/branch, setting layer to 15.");
+          _logger.LogInfo("Found YggdrasilBranch/branch, setting layer to 15.");
           branch.gameObject.layer = 15;
 
-          var filter = branch.GetComponentInChildren<MeshFilter>();
+          MeshFilter filter = branch.GetComponentInChildren<MeshFilter>();
 
-          if (!filter) {
+          if (!filter || !branch.TryGetComponent(out MeshCollider collider)) {
             continue;
           }
 
-          if (!branch.TryGetComponent(out MeshCollider collider)) {
-            ZLog.Log("Adding collider to YggdrasilBranch/branch.");
-            collider = branch.gameObject.AddComponent<MeshCollider>();
-            collider.sharedMesh = filter.sharedMesh;
-          }
+          _logger.LogInfo("Adding collider to YggdrasilBranch/branch.");
+          collider = branch.gameObject.AddComponent<MeshCollider>();
+          collider.sharedMesh = filter.sharedMesh;
         }
 
-        ZLog.Log("Finished FixYggdrasilBranch coroutine.");
+        _logger.LogInfo("Finished FixYggdrasilBranch coroutine.");
         yield break;
       }
     }
