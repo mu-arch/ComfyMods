7b41c1684e46531a39785f2d01948180c2d2e070
redseiko <redseiko@gmail.com>
2021-08-18 00:35:47 -0700

    Temporarily check-in BetterZeeDeeOhs project template.

---
+++ BetterZeeDeeOhs/BetterZeeDeeOhs.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace BetterZeeDeeOhs {
+  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+  public class NewMod : BaseUnityPlugin {
+    public const string PluginGUID = "redseiko.valheim.betterzeedeeohs";
+    public const string PluginName = "BetterZeeDeeOhs";
+    public const string PluginVersion = "1.0.0";
+
+    Harmony _harmony;
+
+    public void Awake() {
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+    }
+
+    public void OnDestroy() {
+      _harmony?.UnpatchSelf();
+    }
+  }
+}
