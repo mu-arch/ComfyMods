0b70921d212421665cc30bf8afcbd32e8ef2189e
redseiko <redseiko@gmail.com>
2021-10-10 15:56:39 -0700

    Create two new mods Compress (WIP) and TwoDayShipping.
      * Compress is prototype code to try compressing/decompressing RPC ZPackage data on both ends.
        * Still requires a means of detecting and supporting vanilla clients.
    
      * TwoDayShipping is a prototype server/client mod that a) optimizes how Server/ClientSortSend methods to avoid creating garbage and b) changes wards send priority to prioritized.

---
+++ Compress/Compress.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using System.Reflection;
+
+namespace Compress {
+  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+  public class Compress : BaseUnityPlugin {
+    public const string PluginGUID = "redseiko.valheim.compress";
+    public const string PluginName = "Compress";
+    public const string PluginVersion = "1.0.0";
+
+    Harmony _harmony;
+
+    public void Awake() {
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+    }
+
+    public void OnDestroy() {
+      _harmony?.UnpatchSelf();
+    }
+
+    [HarmonyPatch(typeof(ZRpc))]
+    class ZRpcPatch {
+      [HarmonyPrefix]
+      [HarmonyPatch(nameof(ZRpc.SendPackage))]
+      static void SendPackagePrefix(ref ZPackage pkg) {
+        byte[] data = Utils.Compress(pkg.GetArray());
+        pkg.Clear();
+        pkg.Write(data);
+      }
+
+      [HarmonyPrefix]
+      [HarmonyPatch(nameof(ZRpc.HandlePackage))]
+      static void HandlePackagePrefix(ref ZPackage package) {
+        package = package.ReadCompressedPackage();
+      }
+    }
+  }
+}
---
+++ TwoDayShipping/README.md
@@ -0,0 +1,26 @@
+﻿# TwoDayShipping
+
+  * Modifies the priority of several prefabs so that they are queued for sending soon than other prefabs.
+    * For now this includes: guard_stone, guard_stone_test
+
+## Details
+
+  * This mod works on both clients and dedicated servers and rewrites the following methods:
+    * `ZDOMan.ServerSortSendZDOS()`
+    * `ZDOMan.ClientSortSendZDOS()`
+    * `ZDOMan.ServerSendCompare()`
+    * `ZDOMan.ClientSendCompare()`
+
+  * Creates a static readonly `Comparison<ZDO>` instance for server and client compares instead of the vanilla
+    behaviour of instantiating a new delegate for each method call.
+
+  * This hooks into the following methods to check if the prefab being added or deserialized is one of the
+    `guard_stone` prefabs and if so, changes the `ZDO.m_type` to `ZDO.ObjectType.Prioritized`.
+    * `ZDOMan.AddToSector()`
+    * `ZDOMan.RPC_ZDOData()`
+
+## Changelog
+
+### 1.0.0
+
+  * Initial release.
