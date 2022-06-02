618c4cc5ba2145f340edddfb1c0d3019a73c7a98
redseiko <redseiko@gmail.com>
2022-06-02 16:56:55 -0700

    Update WIP PartyRock.
      * Move patches into their own files.

---
+++ PartyRock/Patches/TerminalPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace PartyRock {
+  [HarmonyPatch(typeof(Terminal))]
+  public class TerminalPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Terminal.InitTerminal))]
+    static void InitTerminalPostfix() {
+      new Terminal.ConsoleCommand(
+          "playerlist",
+          "PartyRock: toggle the PlayerListPanel.",
+          args => PartyRock.TogglePlayerListPanel());
+      ;
+    }
+  }
+}
