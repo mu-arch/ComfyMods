87be5924ca48228172f2b6818c558636626c660e
redseiko <redseiko@gmail.com>
2022-06-16 21:53:48 -0700

    Add CardHandCardHover.

---
+++ PartyRock/UI/CardHandCardHover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PartyRock {
+  public class CardHandCardHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    RectTransform _rectTransform;
+    int _siblingIndex;
+    Vector3 _lastPosition;
+    Quaternion _lastRotation;
+
+    void Awake() {
+      _rectTransform = GetComponent<RectTransform>();
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData) {
+      _lastPosition = _rectTransform.anchoredPosition;
+      _rectTransform.SetPosition(new(_lastPosition.x, 0f));
+
+      _lastRotation = _rectTransform.rotation;
+      _rectTransform.rotation = Quaternion.identity;
+
+      _siblingIndex = _rectTransform.GetSiblingIndex();
+      _rectTransform.SetAsLastSibling();
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData) {
+      _rectTransform.anchoredPosition = _lastPosition;
+      _rectTransform.rotation = _lastRotation;
+      _rectTransform.SetSiblingIndex(_siblingIndex);
+    }
+  }
+}
