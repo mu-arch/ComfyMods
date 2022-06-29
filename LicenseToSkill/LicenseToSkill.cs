fff160444bf4d0f9e9f1b61a071d6afb36b99d4c
redseiko <redseiko@gmail.com>
2022-06-29 15:16:53 -0700

    Add LicenseToSkill to ComfyMods repo and update it to `v1.1.0`.
      * Moved this mod from private repo to the public ComfyMods repo.
      * Moved all configuration code into new `PluginConfig` class.
      * Moved all Harmony-patching code into their own patch classes.
      * Added `manifest.json`, changed the `icon.png` and updated this `README.md`.
      * Modified the project file to automatically create a versioned Thunderstore package.

---
+++ LicenseToSkill/LicenseToSkill.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using System.Reflection;
+
+using static LicenseToSkill.PluginConfig;
+
+namespace LicenseToSkill {
+  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+  public class LicenseToSkill : BaseUnityPlugin {
+    public const string PluginGUID = "redseiko.valheim.comfytools.licensetoskill";
+    public const string PluginName = "LicenseToSkill";
+    public const string PluginVersion = "1.1.0";
+
+    Harmony _harmony;
+
+    public void Awake() {
+      BindConfig(Config);
+
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    public void OnDestroy() {
+      _harmony?.UnpatchSelf();
+    }
+  }
+}
---
+++ LicenseToSkill/PluginConfig.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx.Configuration;
+
+namespace LicenseToSkill {
+  public class PluginConfig {
+    public static ConfigEntry<bool> IsModEnabled { get; private set; }
+    public static ConfigEntry<float> HardDeathCooldownOverride { get; private set; }
+    public static ConfigEntry<float> SkillLossPercentOverride { get; private set; }
+
+    public static void BindConfig(ConfigFile config) {
+      IsModEnabled = config.Bind("_Global", "isModEnabled", true, "Globally enable or disable this mod.");
+
+      HardDeathCooldownOverride =
+          config.Bind(
+              "OnDeath",
+              "hardDeathCooldownOverride",
+              20f,
+              new ConfigDescription(
+                  "Duration (in minutes) of the 'no skill loss' status effect after death.",
+                  new AcceptableValueRange<float>(10f, 20f)));
+
+      SkillLossPercentOverride =
+          config.Bind(
+              "OnDeath",
+              "skillLossPercentOverride",
+              1f,
+              new ConfigDescription(
+                  "Percentage of the skill's current level to lose on death.",
+                  new AcceptableValueRange<float>(1f, 5f)));
+    }
+  }
+}
