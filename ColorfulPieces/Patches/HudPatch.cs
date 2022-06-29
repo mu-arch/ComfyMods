f8eecfbb835bd7f54e53a4c821577517304d28b5
redseiko <redseiko@gmail.com>
2022-06-28 21:17:45 -0700

    Update ColorfulPieces to `v1.7.0`.
      * Standardized `PluginConfig` to match more recent mods.
      * Moved Harmony patching code into new `PlayerPatch`, `TerminalPatch` and `HudPatch` classes.
      * Terminal commands now check for `IsModEnabled` for initial add and in the commands themselves.
      * Added `manifest.json` and changed `icon.png`.
      * Modified the project file to automatically create a versioned Thunderstore package.

---
+++ ColorfulPieces/Patches/HudPatch.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+
+using UnityEngine;
+
+using static ColorfulPieces.PluginConfig;
+
+namespace ColorfulPieces {
+  [HarmonyPatch(typeof(Hud))]
+  public class HudPatch {
+    static readonly string HoverNameTextTemplate =
+      "{0}{1}"
+          + "<size={9}>"
+          + "[<color={2}>{3}</color>] Set piece color: <color=#{4}>#{4}</color> (<color=#{4}>{5}</color>)\n"
+          + "[<color={6}>{7}</color>] Clear piece color\n"
+          + "[<color={6}>{8}</color>] Copy piece color\n"
+          + "</size>";
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Hud.UpdateCrosshair))]
+    static void HudUpdateCrosshairPostfix(ref Hud __instance, ref Player player) {
+      if (!IsModEnabled.Value || !ShowChangeRemoveColorPrompt.Value || !Player.m_localPlayer.Ref()?.m_hovering) {
+        return;
+      }
+
+      WearNTear wearNTear = player.m_hovering.GetComponentInParent<WearNTear>();
+
+      if (!wearNTear.Ref()?.m_nview || !wearNTear.m_nview.IsValid()) {
+        return;
+      }
+
+      __instance.m_hoverName.text =
+          string.Format(
+              HoverNameTextTemplate,
+              __instance.m_hoverName.text,
+              __instance.m_hoverName.text.Length > 0 ? "\n" : string.Empty,
+              "#FFA726",
+              ChangePieceColorShortcut.Value,
+              ColorUtility.ToHtmlStringRGB(TargetPieceColor.Value),
+              TargetPieceEmissionColorFactor.Value.ToString("N2"),
+              "#EF5350",
+              ClearPieceColorShortcut.Value,
+              CopyPieceColorShortcut.Value,
+              ColorPromptFontSize.Value);
+    }
+  }
+}
---
+++ ColorfulPieces/Patches/TerminalPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+
+using static ColorfulPieces.ColorfulPieces;
+using static ColorfulPieces.PluginConfig;
+
+namespace ColorfulPieces {
+  [HarmonyPatch(typeof(Terminal))]
+  class TerminalPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Terminal.InitTerminal))]
+    static void InitTerminalPostfix() {
+      if (!IsModEnabled.Value) {
+        return;
+      }
+
+      new Terminal.ConsoleCommand(
+          "clearcolor",
+          "ColorfulPieces: Clears all colors applied to all pieces within radius of player.",
+          args => {
+            if (!IsModEnabled.Value
+                || args.Length < 2
+                || !float.TryParse(args.Args[1], out float radius)
+                || !Player.m_localPlayer) {
+              return;
+            }
+
+            args.Context.StartCoroutine(
+                ClearColorsInRadiusCoroutine(Player.m_localPlayer.transform.position, radius));
+          });
+
+      new Terminal.ConsoleCommand(
+          "changecolor",
+          "ColorfulPieces: Changes the color of all pieces within radius of player to the currently set color.",
+          args => {
+            if (!IsModEnabled.Value
+                || args.Length < 2
+                || !float.TryParse(args.Args[1], out float radius)
+                || !Player.m_localPlayer) {
+              return;
+            }
+
+            args.Context.StartCoroutine(
+                ChangeColorsInRadiusCoroutine(Player.m_localPlayer.transform.position, radius));
+          });
+    }
+  }
+}
