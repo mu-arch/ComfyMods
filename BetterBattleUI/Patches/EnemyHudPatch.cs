9d88b8b0d22babdced6c7f2a535e078ea88d5f96
redseiko <redseiko@users.noreply.github.com>
2022-10-22 21:18:18 -0700

    Prototype some text mesh manipulation code like VerticalGradient and WobblyText.
    Sources:
    https://github.com/KPDwyer/TextMeshEffects/blob/master/TextMeshEffects/Assets/TextMeshEffect/Scripts/TextEffects/WobblyTextEffect.cs
    
    https://gist.github.com/dxball/e5e3a292929d633163d4d65f62eb6afc

---
+++ BetterBattleUI/Patches/EnemyHudPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using static BetterBattleUI.PluginConfig;
+
+namespace BetterBattleUI {
+  [HarmonyPatch(typeof(EnemyHud))]
+  static class EnemyHudPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(EnemyHud.Awake))]
+    static void AwakePostfix(ref EnemyHud __instance) {
+      if (!IsModEnabled.Value) {
+        return;
+      }
+
+      // TODO: temporary, just for testing.
+      GameObject bossName = __instance.m_baseHudBoss.transform.Find("Name").gameObject;
+      bossName.AddComponent<VerticalGradient>();
+      bossName.AddComponent<WobblyText>();
+    }
+  }
+}
