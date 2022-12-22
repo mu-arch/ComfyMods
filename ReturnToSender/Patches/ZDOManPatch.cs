e8ee7bcc21ed9c26a7acc01bff0a96b82cb641cd
redseiko <redseiko@gmail.com>
2022-12-22 09:52:51 -0800

    Check in new mod `ReturnToSender`.
    Simple transpiler patch on `ZDOMan.Update()` to revert to the original `SendZDOToPeers()` method.

---
+++ ReturnToSender/Patches/ZDOManPatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using HarmonyLib;
+
+namespace ReturnToSender {
+  [HarmonyPatch(typeof(ZDOMan))]
+  static class ZDOManPatch {
+    [HarmonyTranspiler]
+    [HarmonyPatch(nameof(ZDOMan.Update))]
+    static IEnumerable<CodeInstruction> UpdateTranspiler(IEnumerable<CodeInstruction> instructions) {
+      return new CodeMatcher(instructions)
+          .MatchForward(
+              useEnd: false,
+              new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(ZDOMan), nameof(ZDOMan.SendZDOToPeers2))))
+          .SetOperandAndAdvance(AccessTools.Method(typeof(ZDOMan), nameof(ZDOMan.SendZDOToPeers)))
+          .InstructionEnumeration();
+    }
+  }
+}
---
+++ ReturnToSender/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReturnToSender")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReturnToSender")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("444a0c22-9b14-4246-8053-930ea36381f0")]
+
+[assembly: AssemblyVersion(ReturnToSender.ReturnToSender.PluginVersion)]
+[assembly: AssemblyFileVersion(ReturnToSender.ReturnToSender.PluginVersion)]
