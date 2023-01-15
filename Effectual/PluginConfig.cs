33b479513526e37f7c816ac9adc7e7b339b03a10
redseiko <redseiko@users.noreply.github.com>
2023-01-14 22:07:11 -0800

    Check-in WIP new mod Effectual.

---
+++ Effectual/PluginConfig.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx.Configuration;
+
+using ComfyLib;
+
+namespace Effectual {
+  public static class PluginConfig {
+    public static ConfigEntry<bool> IsModEnabled { get; private set; }
+
+    public static void BindConfig(ConfigFile config) {
+      IsModEnabled =
+          config.BindInOrder(
+              "_Global",
+              "isModEnabled",
+              true,
+              "Globally enable or disable this mod (restart required).");
+    }
+  }
+}
---
+++ Effectual/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Effectual")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Effectual")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("85ae4bbe-4f43-43b5-8d09-db34d48bf2e4")]
+
+[assembly: AssemblyVersion(Effectual.Effectual.PluginVersion)]
+[assembly: AssemblyFileVersion(Effectual.Effectual.PluginVersion)]
