b2dd513d86b4b14057a12ace2b75a64b48367e25
BruceOfTheBow <bruceofthebow@gmail.com>
2023-02-23 19:52:34 -0800

    Update to render boss hud on mobs marked as bosses.

--- Enigma/Patches/EnemyHudPatch.cs
+++ Enigma/Patches/EnemyHudPatch.cs
@@ -2,6 +2,8 @@
 
 using System;
 
+using UnityEngine;
+
 using static Enigma.Enigma;
 using static Enigma.PluginConfig;
 
@@ -12,17 +14,27 @@ namespace Enigma.Patches {
     [HarmonyPostfix]
     [HarmonyPatch(nameof(EnemyHud.ShowHud))]
     public static void ShowHudPostfix(ref EnemyHud __instance, Character c, bool isMount) {
-      if (IsModEnabled.Value 
+      if (IsModEnabled.Value
           && IsBossAnnouncementEnabled.Value
-          && c.TryGetComponent(out MonsterAI monsterAI) 
+          && c.TryGetComponent(out MonsterAI monsterAI)
           && c.TryGetComponent(out ZNetView zNetView)
-          && !zNetView.GetZDO().GetBool(HasSeenFieldName + Player.m_localPlayer.GetPlayerID().ToString(), false) 
+          && !zNetView.GetZDO().GetBool(HasSeenFieldName + Player.m_localPlayer.GetPlayerID().ToString(), false)
           && !string.IsNullOrWhiteSpace(zNetView.GetZDO().GetString(CustomNameFieldName))
-          && zNetView.GetZDO().GetBool(BossDesignationFieldName, false)) { 
+          && zNetView.GetZDO().GetBool(BossDesignationFieldName, false)) {
 
         zNetView.GetZDO().Set(HasSeenFieldName + Player.m_localPlayer.GetPlayerID().ToString(), true);
         MessageHud.instance.ShowBiomeFoundMsg(zNetView.GetZDO().GetString(CustomNameFieldName), false);
       }
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(EnemyHud.TestShow))]
+    public static bool TestShowPretfix(ref EnemyHud __instance, ref bool __result, Character c, bool isVisible) {
+      if(c.TryGetComponent(out ZNetView zNetView) && zNetView.GetZDO().GetBool(BossDesignationFieldName, false) && Vector3.SqrMagnitude(c.transform.position - Player.m_localPlayer.transform.position) < Math.Pow(50,2)) {
+        __result = true;
+        return false;
+      }
+      return true;
     }
   }
 }
