29e5b25cd8400bfd9d7745b701a75454d7928542
BruceOfTheBow <bruceofthebow@gmail.com>
2023-04-15 17:09:41 -0700

    Rebase on main

---
+++ Enigma/Enigma.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using System.Reflection;
+
+using static Enigma.PluginConfig;
+
+namespace Enigma {
+  [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+  public class Enigma : BaseUnityPlugin {
+    public const string PluginGuid = "bruce.valheim.enigma";
+    public const string PluginName = "Enigma";
+    public const string PluginVersion = "1.0.0";
+
+    Harmony _harmony;
+
+    public static readonly string CustomNameFieldName = "customEnemyName";
+
+    public void Awake() {
+      BindConfig(Config);
+
+      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
+    }
+
+    public void OnDestroy() {
+      _harmony?.UnpatchSelf();
+    }
+  }
+}
