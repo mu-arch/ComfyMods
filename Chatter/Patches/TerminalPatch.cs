940f48d8b2559a8e7be37a737cfd76f99787b2a5
redseiko <redseiko@gmail.com>
2023-09-13 15:26:28 -0700

    Check-in more WIP updates for Chatter v2.
    * Add new `TerminalPatch` class that patches the say/shout/whisper Terminal.ConsoleCommand delegates to also set the ChatTextInputDefaultPrefix.

--- Chatter/Patches/TerminalPatch.cs
+++ Chatter/Patches/TerminalPatch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 using HarmonyLib;
 
@@ -17,6 +18,54 @@ namespace Chatter {
               Timestamp = DateTime.Now,
               Text = text,
             });
+      }
+    }
+
+    [HarmonyPatch]
+    static class SayDelegatePatch {
+      [HarmonyTargetMethod]
+      static MethodBase FindSayDelegateMethod() {
+        return AccessTools.Method(AccessTools.Inner(typeof(Terminal), "<>c"), "<InitTerminal>b__7_119");
+      }
+
+      [HarmonyPostfix]
+      static void SayDelegatePostfix(ref object __result) {
+        if (IsModEnabled.Value && (bool) __result == false) {
+          Chatter.ChatterChatPanel?.SetChatTextInputDefaultPrefix(Talker.Type.Normal);
+          __result = true;
+        }
+      }
+    }
+
+    [HarmonyPatch]
+    static class ShoutDelegatePatch {
+      [HarmonyTargetMethod]
+      static MethodBase FindShoutDelegateMethod() {
+        return AccessTools.Method(AccessTools.Inner(typeof(Terminal), "<>c"), "<InitTerminal>b__7_120");
+      }
+
+      [HarmonyPostfix]
+      static void ShoutDelegatePostfix(ref object __result) {
+        if (IsModEnabled.Value && (bool) __result == false) {
+          Chatter.ChatterChatPanel?.SetChatTextInputDefaultPrefix(Talker.Type.Shout);
+          __result = true;
+        }
+      }
+    }
+
+    [HarmonyPatch]
+    static class WhisperDelegatePatch {
+      [HarmonyTargetMethod]
+      static MethodBase FindWhisperDelegateMethod() {
+        return AccessTools.Method(AccessTools.Inner(typeof(Terminal), "<>c"), "<InitTerminal>b__7_121");
+      }
+
+      [HarmonyPostfix]
+      static void WhisperDelegatePostfix(ref object __result) {
+        if (IsModEnabled.Value && (bool) __result == false) {
+          Chatter.ChatterChatPanel?.SetChatTextInputDefaultPrefix(Talker.Type.Whisper);
+          __result = true;
+        }
       }
     }
   }
