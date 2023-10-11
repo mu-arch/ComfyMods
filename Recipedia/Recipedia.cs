35ad9bf15cd969a25b03189f780609fcdef3de33
redseiko <redseiko@gmail.com>
2023-10-11 15:42:29 -0700

    Restore to bare-bones state prototype mod `Recipedia`.

--- Recipedia/Recipedia.cs
+++ Recipedia/Recipedia.cs
@@ -1,12 +1,8 @@
 ﻿using BepInEx;
 
-using ComfyLib;
-
 using HarmonyLib;
 
 using System.Reflection;
-
-using UnityEngine;
 
 using static Recipedia.PluginConfig;
 
@@ -19,36 +15,14 @@ namespace Recipedia {
 
     Harmony _harmony;
 
-    public void Awake() {
+    void Awake() {
       BindConfig(Config);
+
       _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
     }
 
-    public void OnDestroy() {
+    void OnDestroy() {
       _harmony?.UnpatchSelf();
-    }
-
-    public static RecipeListPanel RecipeListPanel { get; private set; }
-
-    public static void ToggleRecipeListPanel() {
-      if (!RecipeListPanel?.Panel) {
-        RecipeListPanel = new(Hud.m_instance.transform);
-
-        RecipeListPanel.Panel.RectTransform()
-            .SetAnchorMin(new(0.5f, 0f))
-            .SetAnchorMax(new(0.5f, 0f))
-            .SetPivot(new(0.5f, 0f))
-            .SetPosition(Vector2.zero)
-            .SetSizeDelta(new(400f, 400f));
-
-        RecipeListPanel.Panel.SetActive(false);
-      }
-
-      ToggleRecipeListPanel(!RecipeListPanel.Panel.activeSelf);
-    }
-
-    public static void ToggleRecipeListPanel(bool toggleOn) {
-      RecipeListPanel?.Panel.Ref()?.SetActive(toggleOn);
     }
   }
 }
